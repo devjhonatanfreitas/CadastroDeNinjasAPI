828df2596702c07dc6168e48ce3c02828abb5542
Jhonatan Freitas <dev.jhonatanfreitas@gmail.com>
2025-07-14 20:57:18 -0300

    Primeiro GetMapping

--- src/main/java/com/jhonatanfreitas/CadastroDeNinjas/Controller.java
+++ src/main/java/com/jhonatanfreitas/CadastroDeNinjas/Controller.java
@@ -1,6 +1,17 @@
 package com.jhonatanfreitas.CadastroDeNinjas;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
 public class Controller {
 
+    @GetMapping("/boasvindas")
+    public String boasVindas (){
+        return "Primeira mensagem de teste de rota";
+    }
 
 }
